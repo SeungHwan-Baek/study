f8306c757cd3c5df0a41bdf2e735aafaf7551951
Jason <ssennom0@naver.com>
2021-03-21 23:01:09 +0900

    JPA 관계설정 추가  - ERD 추가 및 관련 JPA 추가  - JPA 엔티티 연결 설정 추가  - @Builder, @Accessors 추가  - User creat API 추가

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller;
 
 import com.example.study.model.SearchParam;
+import com.example.study.model.network.Header;
 import org.springframework.web.bind.annotation.*;
 
 @RestController                 // Controller로 사용하겠다.
@@ -33,5 +34,15 @@ public class GetController {
 
         // { "accout" : "", "email" : "", "page" :0 }  받은 객체를 return 하면, JSON 으로 변환됨
         return searchParam;
+    }
+
+    @GetMapping("/header")
+    public Header getHeader(){
+
+        // { "resultCode" : "OK", "description" : "OK" }
+        return Header.builder()
+                .resultCode("OK")
+                .description("OK")
+                .build();
     }
 }
---
+++ src/main/java/com/example/study/ifs/CrudInterface.java
@@ -0,0 +1,17 @@
+package com.example.study.ifs;
+
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+
+// 모든 Controller가 동일한 Interface를 상송 받을 수 있도록 Generic으로 정의 함 ( Req, Res로 선언하여 매핑 )
+public interface CrudInterface<Req,Res> {
+
+    Header<Res> create(Header<Req> request);
+
+    Header<Res> read(Long id);
+
+    Header<Res> update(Header<Req> request);
+
+    Header delete(Long id);
+}
