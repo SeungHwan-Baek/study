0e018322f2c9d90b0ce9baec21be05f50b8cb34b
Jason <ssennom0@naver.com>
2021-04-04 18:12:41 +0900

    Controller Class 리팩토링  - CrudController 개발  - ..ApiController 수정

---
+++ src/main/java/com/example/study/controller/CrudController.java
@@ -0,0 +1,40 @@
+package com.example.study.controller;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import org.springframework.web.bind.annotation.*;
+
+/*
+* ItemApiController, OrderGroupApiController, .... Api Controller 반복적인 매핑을 줄이는 추상화 클래스
+* CRUD 서비스 연동을 위한 반복적인 연결을 위함
+* 추상클래스 없이, 각각의 Controller 에서 직접 Service method 매핑 해도 됨.
+*/
+public abstract class CrudController<Req,Res> implements CrudInterface <Req,Res>{
+
+    // Service 연결을 위한 기본서비스
+    protected CrudInterface<Req,Res> baseService;
+
+    @Override
+    @PostMapping("")
+    public Header<Res> create(@RequestBody Header<Req> request) {
+        return baseService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<Res> read(@PathVariable Long id) {
+        return baseService.read(id);
+    }
+
+    @Override
+    @PutMapping("")
+    public Header<Res> update(@RequestBody Header<Req> request) {
+        return baseService.update(request);
+    }
+
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return baseService.delete(id);
+    }
+}
--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,5 +1,6 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.ItemApiRequest;
@@ -9,34 +10,44 @@ import lombok.experimental.Accessors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+// CrudController 추상화 클래스 활용
+//public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")    //  /api/item/
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct
+    public void init(){
+        this.baseService = itemApiLogicService;     // CrudController 의 baseService 연결
     }
 
-    @Override
-    @GetMapping("{id}") // /api/item/i ... 100
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/item
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/item/i ... 100
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
-    }
+    // CrudController 상속으로 주석처리
+//    @Override
+//    @PostMapping("")    //  /api/item/
+//    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}") // /api/item/i ... 100
+//    public Header<ItemApiResponse> read(@PathVariable Long id) {
+//        return itemApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("") // /api/item
+//    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}") // /api/item/i ... 100
+//    public Header delete(@PathVariable Long id) {
+//        return itemApiLogicService.delete(id);
+//    }
 }
